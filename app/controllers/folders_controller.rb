b298ad836c340d8b6874342e97ea930ac98b9672
Pedro <pedro.c.shd@gmail.com>
2020-12-21 13:22:58 -0300

    Refactoring the FoldersController

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -25,40 +25,22 @@ class FoldersController < ApplicationController
   # POST /folders.json
   def create
     @folder = Folder.new(folder_params)
-
-    respond_to do |format|
-      if @folder.save
-        format.html { redirect_to @folder, notice: 'Folder was successfully created.' }
-        format.json { render :show, status: :created, location: @folder }
-      else
-        format.html { render :new }
-        format.json { render json: @folder.errors, status: :unprocessable_entity }
-      end
-    end
+    @folder.save
+    respond_with @folder
   end
 
   # PATCH/PUT /folders/1
   # PATCH/PUT /folders/1.json
   def update
-    respond_to do |format|
-      if @folder.update(folder_params)
-        format.html { redirect_to @folder, notice: 'Folder was successfully updated.' }
-        format.json { render :show, status: :ok, location: @folder }
-      else
-        format.html { render :edit }
-        format.json { render json: @folder.errors, status: :unprocessable_entity }
-      end
-    end
+    @folder.update(folder_params)
+    respond_with @folder
   end
 
   # DELETE /folders/1
   # DELETE /folders/1.json
   def destroy
     @folder.destroy
-    respond_to do |format|
-      format.html { redirect_to folders_url, notice: 'Folder was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    respond_with @folder
   end
 
   private
