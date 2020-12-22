ae7db321137df6921555da080923a7ffa173be82
Pedro <pedro.c.shd@gmail.com>
2020-12-22 09:41:57 -0300

    CRUD Subfolders

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root 'folders#index'
   resources :folders, except: %i[show]
+
+  resources :subfolders, only: %i[edit update destroy show]
+
+  get ':folder_id/subfolders/index', to: 'subfolders#index', as: :index_subfolders
+  get ':folder_id/subfolders/new', to: 'subfolders#new', as: :new_subfolders
+  post ':folder_id/subfolders', to: 'subfolders#create'
 end
