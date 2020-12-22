e12ab219b327752e5a32c2b3d657b3317c12e189
Pedro <pedro.c.shd@gmail.com>
2020-12-22 09:42:46 -0300

    Adjusting on validates Subfolder by Step

--- app/models/subfolder.rb
+++ app/models/subfolder.rb
@@ -1,6 +1,7 @@
 class Subfolder < ApplicationRecord
   belongs_to :folder
-  validates :name, presence: true, uniqueness: true, if: proc {
+  validates :name, presence: true
+  validates :name, uniqueness: true, if: proc {
     SubfoldersQuery.new(name: name, folder_id: folder_id).exists?
   }
 end
