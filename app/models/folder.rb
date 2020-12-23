02bc076bd8a6f4195bcbc1f308d4086d9abe1af1
Pedro <pedro.c.shd@gmail.com>
2020-12-23 12:07:53 -0300

    Folder of the Type Asset (STI)
    Class Folders::Asset is responsible for record file (ActiveStorage 
    Relation)

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -2,6 +2,11 @@ class Folder < ApplicationRecord
   has_many :subfolders, lambda {
     order(:name)
   }, class_name: 'Folders::Subfolder', foreign_key: :parent_id, dependent: :destroy
+
+  has_many :assets, lambda {
+    order(:name)
+  }, class_name: 'Folders::Asset', foreign_key: :parent_id, dependent: :destroy
+
   belongs_to :parent, class_name: 'Folder', optional: true
 
   validates :name, presence: true
