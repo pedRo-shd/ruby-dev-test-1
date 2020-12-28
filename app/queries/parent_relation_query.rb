0947b6f4f0ae90a839aad2a54356653d498f04d3
Pedro <pedro.c.shd@gmail.com>
2020-12-28 17:17:40 -0300

    Query Order by Folder/Subfolder and Asset

--- app/queries/parent_relation_query.rb
+++ app/queries/parent_relation_query.rb
@@ -3,13 +3,13 @@
 class ParentRelationQuery
   attr_reader :parent_relation, :params
 
-  def initialize(params = {}, parent_relation = Folders::Subfolder.includes(:subfolders))
+  def initialize(params = {}, parent_relation = Folder.includes(:subfolders, :assets))
     @parent_relation = parent_relation
     @params = params
   end
 
   def subfolders
-    active_record_relation = parent_relation.where(params)
+    active_record_relation = parent_relation.where(params).order(type: :desc)
     return [] if active_record_relation.blank?
 
     active_record_relation
