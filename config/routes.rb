ff3d92ab564b88f2aa911f74b3dad800a52e9c8d
Pedro <pedro.c.shd@gmail.com>
2020-12-28 17:13:02 -0300

    Upload file to root path or to into Folder/Subfolder

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   resources :folders, except: %i[show]
 
   resources :subfolders, only: %i[edit update destroy show]
-
   get ':parent_id/subfolders/index', to: 'subfolders#index', as: :index_subfolders
   get ':parent_id/subfolders/new', to: 'subfolders#new', as: :new_subfolders
   post ':parent_id/subfolders', to: 'subfolders#create'
+
+  resources :upload_files, only: %i[new create destroy]
 end
