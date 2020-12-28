dfda2370c8507c4cfdd7aa8adb33f08c1e87f868
Pedro <pedro.c.shd@gmail.com>
2020-12-28 17:19:09 -0300

    Available Error Handling to users unless env test.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 require "application_responder"
 
 class ApplicationController < ActionController::Base
-  include ErrorHandling
+  include ErrorHandling unless Rails.env.test?
   self.responder = ApplicationResponder
   respond_to :html, :json
 
